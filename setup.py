5553a8016e1a90541f9f6e04a7432fdc278c6795
Parastoo Taleiniya <taleiniya.sbu@gmail.com>
2021-09-28 14:45:15 +0330

    version 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'yb-client',
-  version = '1.0.0',
+  version = '1.0.1',
   license = 'GNU General Public License v3.0',
   description = 'Yottab-Client is a command-line interface to interact with the Yottab server to create and manage workspaces and applications.',
   author = 'Yottab',
@@ -10,7 +10,7 @@ setup(
   py_modules=['yb_client','yb_client.commands', 'yb_client.file', 'yb_client.api', 'yb_client.output'],
   keywords = ['yottab', 'yottab client', 'command-line'],
   url = 'https://github.com/yottab-io/yb-client',
-  download_url = "https://github.com/yottab-io/yb-client/archive/refs/tags/v1.0.tar.gz",
+  download_url = "https://github.com/yottab-io/yb-client/archive/refs/tags/v1.0.1.tar.gz",
   install_requires=[
     'Click',
     'tabulate',
