d2facf7a15b2685a31d5b015580e6d3060360d37
Parastoo Taleiniya <taleiniya.sbu@gmail.com>
2021-09-28 12:23:44 +0330

    finish setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
   author_email = 'admin@yottab.io',
   py_modules=['yb_client','yb_client.commands'],
   keywords = ['yottab', 'yottab client', 'command-line'],
+  url = 'https://github.com/yottab-io/yb-client',
+  download_url = "https://github.com/yottab-io/yb-client/archive/refs/tags/v1.0.tar.gz",
   install_requires=[
     'Click',
     'tabulate',
@@ -17,4 +19,16 @@ setup(
   entry_points = {
     'console_scripts': ['yb=yb_client.commands:cli'],
   },
+  classifiers = [
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',   
+    'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8'
+  ],
+  
 )
